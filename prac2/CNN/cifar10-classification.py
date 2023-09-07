58281660112147c0868df0d1b2ec0b5394bc0c50
ryanlederhose <ryanlederhose@hotmail.com>
2023-09-07 22:33:03 +1000

    commit

--- prac2/CNN/cifar10-classification.py
+++ prac2/CNN/cifar10-classification.py
@@ -34,16 +34,17 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 3)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 3)
-        self.conv3 = nn.Conv2d(16, 16, 3)
-        self.fc1 = nn.Linear(10816, 120)
+        self.conv3 = nn.Conv2d(16, 32, 3)
+        self.conv4 = nn.Conv2d(32, 32, 3)
+        self.fc1 = nn.Linear(18432, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
-        # for i in range(50):
         x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
         x = torch.flatten(x, 1) # flatten all dimensions except batch
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
