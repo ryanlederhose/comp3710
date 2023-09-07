1df58ced17badc5da0853b48c4c40247bfb21b39
ryanlederhose <ryanlederhose@hotmail.com>
2023-09-07 16:14:57 +1000

    slurm bash file

---
+++ prac2/CNN/slurm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=comp3710
+#SBATCH --nodes=1
+#SBATCH --partition=vgpu
+#SBATCH --gres=gpu:1
+
+source /home/Student/s4583670/miniconda3/bin/activate comp3710
+python3 cnn_lfw.py
