7079440fa5a042725d208c9f0a10eb3713dd06f8
ryanlederhose <ryanlederhose@hotmail.com>
2023-10-11 10:15:50 +1000

    initial report commit

---
+++ report/vision_transformer_ryanlederhose/train.py
@@ -0,0 +1,144 @@
+import argparse
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision.transforms import Compose, ToTensor, Resize
+from torch import optim
+import numpy as np
+from torch.hub import tqdm
+from dataset import DataLoader
+from modules import PatchExtractor, InputEmbedding, ViT, EncoderBlock
+
+class TrainEval:
+
+    def __init__(self, args, model, train_dataloader, val_dataloader, optimizer, criterion, device):
+        self.model = model
+        self.train_dataloader = train_dataloader
+        self.val_dataloader = val_dataloader
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.epoch = args.epochs
+        self.device = device
+        self.args = args
+
+    def train_fn(self, current_epoch):
+        self.model.train()
+        total_loss = 0.0
+        tk = tqdm(self.train_dataloader, desc="EPOCH" + "[TRAIN]" + str(current_epoch + 1) + "/" + str(self.epoch))
+
+        for t, data in enumerate(tk):
+            images, labels = data
+            images, labels = images.to(self.device), labels.to(self.device)
+            self.optimizer.zero_grad()
+            logits = self.model(images)
+            loss = self.criterion(logits, labels)
+            loss.backward()
+            self.optimizer.step()
+
+            total_loss += loss.item()
+            tk.set_postfix({"Loss": "%6f" % float(total_loss / (t + 1))})
+            if self.args.dry_run:
+                break
+
+        return total_loss / len(self.train_dataloader)
+
+    def eval_fn(self, current_epoch):
+        self.model.eval()
+        total_loss = 0.0
+        tk = tqdm(self.val_dataloader, desc="EPOCH" + "[VALID]" + str(current_epoch + 1) + "/" + str(self.epoch))
+
+        for t, data in enumerate(tk):
+            images, labels = data
+            images, labels = images.to(self.device), labels.to(self.device)
+
+            logits = self.model(images)
+            loss = self.criterion(logits, labels)
+
+            total_loss += loss.item()
+            tk.set_postfix({"Loss": "%6f" % float(total_loss / (t + 1))})
+            if self.args.dry_run:
+                break
+
+        return total_loss / len(self.val_dataloader)
+
+    def train(self):
+        best_valid_loss = np.inf
+        best_train_loss = np.inf
+        for i in range(self.epoch):
+            train_loss = self.train_fn(i)
+            val_loss = self.eval_fn(i)
+
+            if val_loss < best_valid_loss:
+                torch.save(self.model.state_dict(), "best-weights.pt")
+                print("Saved Best Weights")
+                best_valid_loss = val_loss
+                best_train_loss = train_loss
+        print(f"Training Loss : {best_train_loss}")
+        print(f"Valid Loss : {best_valid_loss}")
+
+    '''
+        On default settings:
+        
+        Training Loss : 2.3081023390197752
+        Valid Loss : 2.302861615943909
+        
+        However, this score is not competitive compared to the 
+        high results in the original paper, which were achieved 
+        through pre-training on JFT-300M dataset, then fine-tuning 
+        it on the target dataset. To improve the model quality 
+        without pre-training, we could try training for more epochs, 
+        using more Transformer layers, resizing images or changing 
+        patch size,
+    '''
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Vision Transformer in PyTorch')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--patch-size', type=int, default=16,
+                        help='patch size for images (default : 16)')
+    parser.add_argument('--latent-size', type=int, default=768,
+                        help='latent size (default : 768)')
+    parser.add_argument('--n-channels', type=int, default=3,
+                        help='number of channels in images (default : 3 for RGB)')
+    parser.add_argument('--num-heads', type=int, default=12,
+                        help='(default : 16)')
+    parser.add_argument('--num-encoders', type=int, default=12,
+                        help='number of encoders (default : 12)')
+    parser.add_argument('--dropout', type=int, default=0.1,
+                        help='dropout value (default : 0.1)')
+    parser.add_argument('--img-size', type=int, default=224,
+                        help='image size to be reshaped to (default : 224')
+    parser.add_argument('--num-classes', type=int, default=2,
+                        help='number of classes in dataset (default : 10 for CIFAR10)')
+    parser.add_argument('--epochs', type=int, default=10,
+                        help='number of epochs (default : 10)')
+    parser.add_argument('--lr', type=float, default=1e-2,
+                        help='base learning rate (default : 0.01)')
+    parser.add_argument('--weight-decay', type=int, default=3e-2,
+                        help='weight decay value (default : 0.03)')
+    parser.add_argument('--batch-size', type=int, default=4,
+                        help='batch size (default : 4)')
+    parser.add_argument('--dry-run', action='store_true', default=False,
+                        help='quickly check a single pass')
+    args = parser.parse_args()
+
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    dl = DataLoader(batch_size=args.batch_size)
+    train_loader = dl.get_training_loader()
+    test_loader = dl.get_test_loader()
+
+    model = ViT(args).to(device)
+
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    criterion = nn.CrossEntropyLoss()
+
+    TrainEval(args, model, train_loader, test_loader, optimizer, criterion, device).train()
+
+
+if __name__ == "__main__":
+    main()
